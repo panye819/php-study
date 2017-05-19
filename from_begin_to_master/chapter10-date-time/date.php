25cf47e042f9d596c3357713df35ffd83ecedee7
panye819 <panye819@gmail.com>
2017-05-19 10:15:21 +0800

    05190945

--- from_begin_to_master/chapter10-date-time/date.php
+++ from_begin_to_master/chapter10-date-time/date.php
@@ -15,3 +15,29 @@
     echo "当前的时间为：".date("H:i:s",mktime())."<p>";
 
     //比较两个时间的大小
+    $time1 = date("Y-m-d H:i:s");
+    $time2 = "2008-2-3 16:30:00";
+    echo "变量\$time1的时间为：".$time1."<BR>";
+    echo "变量\$time2的时间为：".$time2."<BR>";
+
+    if (strtotime($time1)- strtotime($time2) < 0){
+        echo "\$time1 早于 \$time2";
+    }else{
+        echo "\$time2 早于 $time1";
+
+    }
+
+    //实现倒计时功能
+    $time3 = strtotime(date("Y-m-d H:i:s"));
+    $time4 = strtotime("2017-5-19 18:00:00");
+    $time5 = strtotime("2018-1-1");
+    echo "<BR>";
+    echo $time4 - $time3;
+    echo "<BR>";
+    echo $time5 - $time3;
+    echo "<BR>";
+    $sub1 = ceil(($time4-$time3)/3600);
+    $sub2 = ceil(($time5-$time3)/86400);
+    echo "距离本周周末还有<font color='red'> $sub1</font>小时";
+    echo "<BR>";
+    echo "离2018年元旦还有<font color='red'> $sub2</font>天";
