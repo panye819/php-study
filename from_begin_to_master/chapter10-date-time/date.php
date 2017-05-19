82a3b8a11d256fc8c90cc386e447e0ca7379a2dd
castlepan <castlepan@chainresearch.org>
2017-05-19 09:44:43 +0800

    05190945

--- from_begin_to_master/chapter10-date-time/date.php
+++ from_begin_to_master/chapter10-date-time/date.php
@@ -1,7 +1,17 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: hx-tech-08
- * Date: 2017/5/18
- * Time: 下午6:08
+ * 全球分为24个时区，每个时区都有自己的本地时间。
+ * 在国际无线电通用领域，使用一个统一的时间，称为通用协调时间（Universal Time Coordinated UTC），UTC时间与格林威治标准时间GMT相同
+ * 都与英国伦敦的本地时间相同。
+ *
+ * PHP中默认设置是GMT。
+ *
+ *
  */
+
+    //获得本地时间戳
+    echo "mktime 函数返回的时间戳：".mktime()."<p>";
+    echo "当前的日期为：".date("Y-m-d",mktime())."<p>";
+    echo "当前的时间为：".date("H:i:s",mktime())."<p>";
+
+    //比较两个时间的大小
