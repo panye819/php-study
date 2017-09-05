610ea42d5258fbf91da9adc1de969e4aa880a759
panye819 <panye819@gmail.com>
2017-09-05 09:18:18 +0800

    09050918

---
+++ itcast-php-38/00-demo/demo4.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: castlepan
+ * Date: 2017/8/10
+ * Time: 上午9:58
+ */
+
+//echo 'I am a boy<br>';
+//echo 'She is a girl';
+echo "Today is ".date("l").". ";
